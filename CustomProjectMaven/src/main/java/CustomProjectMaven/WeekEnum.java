dcc5c75c7c7b6813807ace6fb2d0cdb0765b21bc
Ariel Xin <yuexin@cs.ucla.edu>
2017-05-18 18:25:36 -0700

    added pure enum

---
+++ CustomProjectMaven/src/main/java/CustomProjectMaven/WeekEnum.java
@@ -0,0 +1,14 @@
+package CustomProjectMaven;
+
+/**
+ * Created by arielxin on 5/18/17.
+ */
+public enum WeekEnum {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
