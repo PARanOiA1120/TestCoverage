190fa9066392865263c4c264810758f130b5a79d
vincccho <vinccc118@gmail.com>
2017-06-01 16:41:15 -0700

    modify exception test

--- CustomProjectMaven/src/main/java/CustomProjectMaven/UseExceptionClass.java
+++ CustomProjectMaven/src/main/java/CustomProjectMaven/UseExceptionClass.java
@@ -6,8 +6,10 @@ package CustomProjectMaven;
 public class UseExceptionClass {
     public UseExceptionClass(String type) throws ExceptionClassA, ExceptionClassB
     {
-        if(type.equals("A"))
+        if(type.equals("A")){
             throw new ExceptionClassA("Throw ExceptionA");
+			System.out.println("this line will not execute");
+		}
         else if(type.equals("B"))
             throw new ExceptionClassB("Throw ExceptionB");
     }
